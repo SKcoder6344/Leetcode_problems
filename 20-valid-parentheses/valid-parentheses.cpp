48f6aa316a4cb891bd597c6b89b114c1349742d3
Sujal Kumar  Nayak <nayaksujalkumar@gmail.com>
2026-02-17 00:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.72%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        map<char,char> pairs={
+            {')','('},
+            {'}','{'},
+            {']','['},
+        };
+
+        for (char c : s){
+            if (pairs.count(c)){
+                if(st.empty() || st.top() != pairs[c]){
+                    return false;
+                }
+                st.pop();
+            } else{
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
